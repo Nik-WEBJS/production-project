15d2a7b072f5dd18c60860b7b8328fed14ddd8a4
Nikita Fokin <nikita.fokin0123@gmail.com>
2025-02-25 05:30:57 +0500

    create helpers class

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,12 @@ import { Link } from "react-router-dom";
 import { AboutPageAsync } from "./pages/AboutPage/AboutPage.async";
 import { MainPageAsync } from "./pages/MainPage/MainPage.async";
 import { useTheme } from "./theme/useTheme";
+import { classNames } from "./helpers/classNames/classNames";
 
 const App: React.FC = () => {
   const { theme, toggleTheme } = useTheme();
-
   return (
-    <div className={`app ${theme}`}>
+    <div className={classNames('app', {hovered: true, selected: false}, [theme, 'clas2'] )}>
       <button onClick={toggleTheme}>Toggle</button>
       <Link to={"/"}>Главная</Link>
       <Link to={"/about"}>О нас</Link>
